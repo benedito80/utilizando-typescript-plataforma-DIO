49efcebeb22bc265ed1ea63991e3432520badc4d
benedito80 <benedito.ufpi.oeiras@gmail.com>
2022-08-04 23:16:23 -0300

    one commit

---
+++ dist/index.js
@@ -0,0 +1,56 @@
+"use strict";
+let button = document.getElementById("button");
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let botaoAtualizar = document.getElementById("atualizar-saldo");
+let botaoLimpar = document.getElementById("limpar-saldo");
+let soma = document.getElementById("soma");
+let campoSaldo = document.getElementById("campo-saldo");
+let saldoTotal = 0;
+function somar(n1, n2) {
+    return n1 + n2;
+}
+if (button) {
+    button.addEventListener("click", () => {
+        if (input1 && input2) {
+            let res = somar(Number(input1.value), Number(input2.value));
+            saldoTotal = res;
+            campoSaldo.innerHTML = res.toString();
+            limpaInputs();
+        }
+    });
+}
+limparSaldo();
+function somarAoSaldo(soma) {
+    if (campoSaldo) {
+        saldoTotal += soma;
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+function limparCampoSoma() {
+    soma.value = "";
+    limpaInputs();
+}
+function limpaInputs() {
+    if (input1 && input2) {
+        input1.value = "";
+        input2.value = "";
+    }
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTotal = 0;
+        soma.value = "";
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener("click", () => {
+        somarAoSaldo(Number(soma.value));
+    });
+}
+botaoLimpar.addEventListener("click", () => {
+    limparSaldo();
+    limpaInputs();
+});
